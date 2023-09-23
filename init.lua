ff3725d28c1d7bee0e3002cb9d281c29f33d6ecf
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-09-24 00:03:18 +0200

    Write the default values since it's more clearly broken that way

--- init.lua
+++ init.lua
@@ -9,10 +9,10 @@ for element in xml:each_child() do
 
 	-- element.attr.solid_gravity_scale = (element.attr.solid_gravity_scale or 1) * -1
 
-	element.attr.gas_speed = "23"
-	element.attr.gas_upwards_speed = "200"
-	element.attr.gas_downwards_speed = "240"
-	element.attr.gas_horizontal_speed = "255"
+	element.attr.gas_speed = "50"
+	element.attr.gas_upwards_speed = "100"
+	element.attr.gas_downwards_speed = "90"
+	element.attr.gas_horizontal_speed = "100"
 
 end
 
